b5789248d79f6b8d579f26013bf7838c9b028cd7
sujit gargate <sujitgargate5893@gmail.com>
2020-07-13 19:09:37 +0530

    Calculation for 20 days Employee Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -129,5 +129,37 @@ echo $salary
 
 ## This is end Employee Switch case code ##
 
+## calculate Employee 20 days wage ##
+
+#!/bin/bash -x
+isPartTime=1;
+isFullTime=2;
+maxHrsInMonth=10;
+empRatePerHour=30;
+
+numberOfWorkingDays=20;
+totalEmpHrs=0;
+totalWorkingDays=20;
+
+while [[ $totalEmpHrs -lt $maxHrsInMonth && $totalWorkingDays -lt $numberOfWorkingDays ]]
+do
+((totalWorkingDays++))
+empCheck=$((RANDOM%3))
+case $empCheck in
+        $isFullTime)
+                empHours=8
+                ;;
+        $isPartTime)
+                empHours=4
+                ;;
+        *)
+                empHours=0
+                ;;
+esac
+totalEmpHrs=$(($totalEmpHrs*$empRatePerHour))
+done
+totalSalary=$(($totalEmpHrs * $empRatePerHour))
 
 
+
+## End of calculate Employee 20 days wage ##
