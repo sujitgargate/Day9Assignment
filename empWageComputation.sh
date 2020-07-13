fb9139d9d5b6fde18cb780307d8c80d82b49a4c1
sujit gargate <sujitgargate5893@gmail.com>
2020-07-13 19:17:00 +0530

    Refactored Code Using Functions

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -167,6 +167,76 @@ totalSalary=$(($totalEmpHrs * $empRatePerHour))
 
 ## Limiting to the Employee workings days and workign hours
 
+#!/bin/bash -x
+isPartTime=1;
+isFullTime=2;
+maxHrsInMonth=100;
+empRatePerHour=20;
 
+numberOfWorkingDays=20;
+totalEmpHrs=0;
+totalWorkingDays=20;
+
+while [[ $totalEmpHrs -lt $maxHrsInMonth && $totalWorkingDays -lt $numberOfWorkingDays ]]
+do
+((totalWorkingDays++))
+empCheck=$((RANDOM%3))
+case $empCheck in
+        $isFullTime)
+                empHours=8
+                ;;
+        $isPartTime)
+                empHours=4
+                ;;
+        *)
+                empHours=0
+                ;;
+esac
+totalEmpHrs=$(($totalEmpHrs*$empRatePerHour))
+done
+totalSalary=$(($totalEmpHrs * $empRatePerHour))
 
 ## End of Limiting to the Employee workings days and workign hours
+
+
+## End of Refactor Code using Functions ##
+
+#!/bin/bash -x
+isPartTime=1;
+isFullTime=2;
+maxHrsInMonth=10;
+empRatePerHour=30;
+totalEmpHrs=0;
+
+numberOfWorkingDays=20;
+totalWorkingDays=0;
+
+function getWorkingHours()
+{
+	#local $empCheck=$1
+	case $empCheck in
+        $isFullTime)
+                empHours=8
+                ;;
+        $isPartTime)
+                empHours=4
+                ;;
+        *)
+                empHours=0
+                ;;
+esac
+echo $empHours
+
+}
+while [[ $totalEmpHrs -lt $maxHrsInMonth && 
+				$totalWorkingDays -lt $numberOfWorkingDays ]]
+do
+	((totalWorkingDays++))
+	empCheck=$((RANDOM%2))
+	empHours="$( getWorkingHours $empCheck )"
+	totalEmpHrs=$(($totalEmpHrs + $empHours))
+done
+totalSalary=$(($totalEmpHrs * $empRatePerHour))
+
+
+## End of Refactor Code using Functions ##
