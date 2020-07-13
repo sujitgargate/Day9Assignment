579bc13785f3ef67f906536e8a9ff46796ec62bf
sujit gargate <sujitgargate5893@gmail.com>
2020-07-13 19:06:22 +0530

    Employee Wage Solved using Switch case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -103,3 +103,31 @@ echo ${dailyWage[@]}
 
 
 ##This is end of Employee Part Time code ##
+
+## This is Employee Switch case code ##
+
+#!/bin/bash -x
+employeeCheck=$((RANDOM%3));
+empRatePerHour=20;
+isFullTime=2;
+isPartTime=1;
+for (counter)
+case $employeeCheck in 
+	$isFullTime)
+		empHours=8
+		;;
+	$isPartTime)
+		empHours=4
+		;;
+	*)
+		empHours=0
+		;;
+esac
+salary=$(($empHours*$empRatePerHour))
+echo $salary
+
+
+## This is end Employee Switch case code ##
+
+
+
