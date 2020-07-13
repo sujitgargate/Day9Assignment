a15c67a82917b8c85f3cb925081580e5f6ce81ff
sujit gargate <sujitgargate5893@gmail.com>
2020-07-13 18:57:16 +0530

    Employee daily wage code added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -11,3 +11,50 @@ else
 fi
 
 ## This is end of Employee present or absent file ##
+
+## This is Employee daily wage find code##
+
+
+#!/bin/bash -x 
+isFullTime=2
+maxHrsInMonth=10;
+empRatePerHour=20;
+totalEmpHrs=0;
+
+numberOfWorkingDays=20;
+totalWorkingDays=0;
+function getEmpWage()
+{
+	local empWage=$1;
+	empHours=$(( $empWage * $empRatePerHour ))
+	echo $(($empHours))
+}
+
+function getWorkingHours()
+{
+	local empCheck=$1
+	case $empCheck in
+        $isFullTime)
+                empHours=8
+                ;;
+        *)
+                empHours=0
+                ;;
+esac
+echo $empHours
+}
+while [[ $totalEmpHrs -lt $maxHrsInMonth && 
+				$totalWorkingDays -lt $numberOfWorkingDays ]]
+do
+	((totalWorkingDays++))
+	#empCheck=$((RANDOM%3))
+	empCkeck=1
+	empHours="$(getWorkingHours $empCheck)"
+	totalEmpHrs=$(( $totalEmpHrs + $empHours ))
+	dailyWage[$totalWorkingDays]=$(getEmpWage $empHours)
+done
+totalSalary=$(($totalEmpHrs * $empRatePerHour))
+echo ${dailyWage[@]}
+
+## This is end of Employee daily wage find code##
+
