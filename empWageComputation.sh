e6e32a0f4288ca6f3b1fc5f8c59d9588d13a666c
sujit gargate <sujitgargate5893@gmail.com>
2020-07-13 19:20:32 +0530

    Storing Employee Daily wage and total wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -240,3 +240,52 @@ totalSalary=$(($totalEmpHrs * $empRatePerHour))
 
 
 ## End of Refactor Code using Functions ##
+
+## Storing Daily Wage and total Wage of Employee ##
+
+#!/bin/bash -x
+isPartTime=1;
+isFullTime=2;
+maxHrsInMonth=10;
+empRatePerHour=30;
+totalEmpHrs=0;
+
+numberOfWorkingDays=20;
+totalWorkingDays=0;
+function getEmpWage()
+{
+	local empWage=$1;
+	empHours=$(( $empWage * $empRatePerHour ))
+	echo $(($empHours))
+}
+
+function getWorkingHours()
+{
+	local empCheck=$1
+	case $empCheck in
+        $isFullTime)
+                empHours=8
+                ;;
+        $isPartTime)
+                empHours=4
+                ;;
+        *)
+                empHours=0
+                ;;
+esac
+echo $empHours
+}
+while [[ $totalEmpHrs -lt $maxHrsInMonth && 
+				$totalWorkingDays -lt $numberOfWorkingDays ]]
+do
+	((totalWorkingDays++))
+	empCheck=$((RANDOM%3))
+	empHours="$(getWorkingHours $empCheck)"
+	totalEmpHrs=$(( $totalEmpHrs + $empHours ))
+	dailyWage[$totalWorkingDays]=$(getEmpWage $empHours)
+done
+totalSalary=$(($totalEmpHrs * $empRatePerHour))
+echo ${dailyWage[@]}
+
+
+## End of Storing Daily Wage and total Wage of Employee ##
