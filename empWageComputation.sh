5322d5fb6705738c3539fbbe978d119f63c9c9e6
sujit gargate <sujitgargate5893@gmail.com>
2020-07-13 19:13:44 +0530

    Adding limit to the employee hours and working days

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -167,6 +167,35 @@ totalSalary=$(($totalEmpHrs * $empRatePerHour))
 
 ## Limiting to the Employee workings days and workign hours
 
+#!/bin/bash -x
+isPartTime=1;
+isFullTime=2;
+maxHrsInMonth=100;
+empRatePerHour=20;
+
+numberOfWorkingDays=20;
+totalEmpHrs=0;
+totalWorkingDays=20;
+
+while [[ $totalEmpHrs -lt $maxHrsInMonth && $totalWorkingDays -lt $numberOfWorkingDays ]]
+do
+((totalWorkingDays++))
+empCheck=$((RANDOM%3))
+case $empCheck in
+        $isFullTime)
+                empHours=8
+                ;;
+        $isPartTime)
+                empHours=4
+                ;;
+        *)
+                empHours=0
+                ;;
+esac
+totalEmpHrs=$(($totalEmpHrs*$empRatePerHour))
+done
+totalSalary=$(($totalEmpHrs * $empRatePerHour))
+
 
 
 ## End of Limiting to the Employee workings days and workign hours
